0a757506d737e8cfc8fdff7049ec6bb7cce78495
Andrew McAleavey <=>
2022-06-03 11:31:41 +0200

    Adding fscores.R and modifying .gitignore

---
+++ R/fscores.R
@@ -0,0 +1,12 @@
+#' SCORING using factor-standardized scoring system
+#'
+#' Ideally, this will use publically-available scoring data to score
+#' standardized versions of each NF2.0 scale.
+#'
+#' STEPS:
+#' 1. Identify optimal (graded response model) models for scoring each subscale in a way
+#' that is consistent with clinical use and allows score computation with missing
+#' item-level data.
+#' 2. Include those models in this package
+#' 3. Provide functions to compute factor scores from item-level data
+#' 4. Compute factor scores from item-level data in the real and synthetic data.
